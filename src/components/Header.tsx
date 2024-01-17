9277baf521d2405fc2ac6ef341ad59c703c336bb
Alya Kovalenko <kovalenkoalinam@gmail.com>
2024-01-17 18:56:11 +0300

    fix: logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Logo from './Logo';
 
 export default function Header() {
   return (
-    <header className='sticky top-0 grid bg-orange-100/85 shadow-md shadow-orange-100/65 min-h-14'>
+    <header className='sticky top-0 flex bg-orange-100/85 shadow-md shadow-orange-100/65 min-h-14 h-14'>
       <Logo />
     </header>
   )
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image';
+import logo from '../../public/images/logo.svg';
+
 export default function Logo() {
   return (
-    <section className='text-orange-500 relative flex justify-center items-center'>
-      <span className='font-["Open_Sans"] text-4xl font-semibold'>W</span>
-      <span className='font-mono text-[10px] leading-3 absolute top-1/5 bg-orange-100/85'>WISHLIST</span>
+    <section>
+      <Image src={logo} alt='logo' className='h-full object-contain' />
     </section>
   )
 }
