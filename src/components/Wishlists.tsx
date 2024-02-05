d832e5d05dcf86651299c7960832611954dc9343
Alya Kovalenko <kovalenkoalinam@gmail.com>
2024-02-05 15:36:22 +0300

    feat: add fetch with redux rtq

--- src/components/Wishlists.tsx
+++ src/components/Wishlists.tsx
@@ -1,16 +1,26 @@
-import mockData from '@/assets/data/wishListData.json';
+'use client';
+
+import { useGetWishlistsByUserQuery } from '@/services/wishlist';
 import WishlistItem from './WishlistItem';
 
 export default function Wishlists() {
+  const { data, error, isLoading } = useGetWishlistsByUserQuery(3);
+
   return (
     <>
-      {mockData.users[0].wishlists.map((item) => (
-        <WishlistItem
-          key={item.id}
-          wishlist={item.title}
-          wishlistId={item.id}
-        />
-      ))}
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : (
+        data.map((item: { id: number; user_id: number; title: string }) => (
+          <WishlistItem
+            key={item.id}
+            wishlist={item.title}
+            wishlistId={item.id}
+          />
+        ))
+      )}
     </>
   );
 }
